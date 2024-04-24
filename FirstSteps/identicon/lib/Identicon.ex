8770827122da3abada41a621120ed03a44eba3b0
Desktop PC <andrej.zhdkv@gmail.com>
2024-04-24 14:04:19 +0200

    Add Erlang image generator

--- FirstSteps/identicon/lib/Identicon.ex
+++ FirstSteps/identicon/lib/Identicon.ex
@@ -4,6 +4,38 @@ defmodule Identicon do
       |> hash_input
       |> pick_color
       |> build_grid
+      |> filter_odd_squares
+      |> build_pixel_map
+  end
+"""
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+      Enum.map grid, fn({_code, index}) -> 
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+  end
+"""
+
+  def build_pixel_map(grid) do
+    Enum.map(grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end)
+  end
+
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter(grid, fn({code, _index}) -> rem(code, 2) == 0 end)
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
@@ -32,5 +64,4 @@ defmodule Identicon do
 
     %Identicon.Image{hex: hex}
   end
-
 end
